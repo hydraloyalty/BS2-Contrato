68dfc88bf813e8093e2e785a929508979113843a
Hydra <github@equipehydra.com>
2024-06-10 00:09:17 -0300

    Atualização com minuta

--- Scripts/imprimir.js
+++ Scripts/imprimir.js
@@ -1,51 +1,41 @@
- document.addEventListener('DOMContentLoaded', function () {
-
-    var botaoImprimir = document.getElementById('botao-imprimir');
-    var secaoConfig = document.querySelector('.config');
-	var documento = document.getElementById('documento');
-    var capa = document.getElementById('capa');
-	var pagina1 = document.getElementById('pagina1');
-	var pagina2 = document.getElementById('pagina2');
-	var pagina3 = document.getElementById('pagina3');
-	var contracapa = document.getElementById('contracapa');
+document.addEventListener('DOMContentLoaded', function () {
+    const botaoImprimir = document.getElementById('botao-imprimir');
+    const secaoConfig = document.querySelector('.config');
+    const documento = document.getElementById('documento');
+    const capa = document.getElementById('capa');
+	const pagina1 = document.getElementById('pagina1');
+	const pagina2 = document.getElementById('pagina2');
+	const pagina3 = document.getElementById('pagina3');
+	const contracapa = document.getElementById('contracapa');
 
     var inputsConfig = document.querySelectorAll('.input-config');
 
-    // Função para verificar se todos os inputs estão preenchidos
-    function verificarInputsPreenchidos() {
-        var todosPreenchidos = true;
-        inputsConfig.forEach(function (input) {
-            if (!input.value) {
-                todosPreenchidos = false;
-            }
-        });
-        return todosPreenchidos;
-    }
-
-    // Função para lidar com a validação dos inputs
-    function validarInputs() {
-        var todosPreenchidos = verificarInputsPreenchidos();
-        if (todosPreenchidos) {
-            botaoImprimir.disabled = false; // Habilita o botão de exportar se todos os inputs estiverem preenchidos
+    const verificarInputsPreenchidos = () => Array.from(inputsConfig).every(input => input.value);
+    const validarInputs = () => {
+        if (verificarInputsPreenchidos()) {
+            botaoImprimir.classList.add('botao-habilitado');
+            botaoImprimir.classList.remove('botao-desabilitado');
         } else {
-            botaoImprimir.disabled = true; // Desabilita o botão de exportar se algum input não estiver preenchido
+            botaoImprimir.classList.add('botao-desabilitado');
+            botaoImprimir.classList.remove('botao-habilitado');
         }
-    }
+    };
 
-    // Adiciona um listener de evento a cada input para validar quando o conteúdo muda
-    inputsConfig.forEach(function (input) {
-        input.addEventListener('input', validarInputs);
-    });
+    inputsConfig.forEach(input => input.addEventListener('input', validarInputs));
 
     if (botaoImprimir && secaoConfig) {
-        botaoImprimir.addEventListener('click', function () {
-            // Adiciona uma classe para ocultar a seção de configuração
+        botaoImprimir.addEventListener('click', () => {
+            if (!verificarInputsPreenchidos()) {
+                alert('Você precisa preencher todos os campos para exportar esse documento.');
+                return;
+            }
+
             secaoConfig.classList.add('ocultar-na-impressao');
-			
-			documento.style.gridTemplateColumns  = "auto";
-			documento.style.gap = "0px";
-			
-			var elements = [capa, pagina1, pagina2, pagina3, contracapa];
+
+            documento.style.gridTemplateColumns = "auto";
+            documento.style.gap = "0px";
+
+            var elements = [capa, pagina1, pagina2, pagina3, contracapa];
             elements.forEach(function (el) {
                 el.style.border = "0px solid #00003F";
             });
@@ -56,16 +46,16 @@
             // Espera um pouco antes de remover a classe (por exemplo, 500 milissegundos)
             setTimeout(function () {
                 secaoConfig.classList.remove('ocultar-na-impressao');
-				
-				documento.style.gridTemplateColumns = "auto auto";
-				documento.style.gap = "2px";
-				
-				elements.forEach(function (el) {
+
+                documento.style.gridTemplateColumns = "auto auto";
+                documento.style.gap = "2px";
+
+                elements.forEach(function (el) {
                     el.style.border = "2px solid #00003F";
                 });
             }, 500);
-			
-			 // Adiciona ou remove a classe 'exportado' ao botão de exportar
+
+            // Adiciona ou remove a classe 'exportado' ao botão de exportar
             botaoImprimir.classList.toggle('exportado');
         });
 
